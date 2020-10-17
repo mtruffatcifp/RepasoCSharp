b7627cccdcc56bd16c393cc59a6fcb93253fef1e
Marco <marcotruffat@paucasesnovescifp.cat>
2020-10-17 21:23:45 +0200

    Finished Exercise1_ForeachAndLists.
    Created a string list filled with persons first names. Also created a object in a new class (PersonModel) with  FirstName and LastName as properties. Then created an object for each person. At execute it shows first the string lista and then the object list.

--- Exercise1_ForeachAndLists/Program.cs
+++ Exercise1_ForeachAndLists/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Exercise1_ForeachAndLists
@@ -10,6 +12,31 @@ namespace Exercise1_ForeachAndLists
     {
         static void Main(string[] args)
         {
+            var personas = new List<string> { "Tanya", "Miriam", "Glenn", "Xavier", "Musa", "Ryan", "Evangeline", "Otis", "Tyler" };
+
+            foreach (var pers in personas)
+              Console.WriteLine("Hello " + pers);
+            Console.WriteLine();
+
+            List<PersonModel> personasModel = GetpeopleModels();
+
+            foreach (var persona in personasModel)
+                Console.WriteLine("Hello " + persona.FirstName + " " +  persona.LastName);
+
+            Console.WriteLine();
+            Console.Write("Press enter to close...");
+            Console.ReadLine();
+        }
+        private static List<PersonModel> GetpeopleModels()
+        {
+            var personas = new List<PersonModel>();
+
+            personas.Add(new PersonModel { FirstName = "Becky", LastName = "Clayton" });
+            personas.Add(new PersonModel { FirstName = "Alessandro", LastName = "Correa" });
+            personas.Add(new PersonModel { FirstName = "Aubrey", LastName = "Swift" });
+            personas.Add(new PersonModel { FirstName = "Stella", LastName = "Whittle" });
+
+            return personas;
         }
     }
 }
