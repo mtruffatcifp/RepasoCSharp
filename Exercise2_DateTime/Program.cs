05aef071eff5fbb2fc58354309e523f264030741
Marco <marcotruffat@paucasesnovescifp.cat>
2020-10-21 20:17:30 +0200

    Finished Exercise2_DateTime.
    Program ruturns total of days passed from introduced date. After that asks for a time, it returns how many hours and minutes passed from that time. If time is greater than actual then it takes it as a yesterday time.

--- Exercise2_DateTime/Program.cs
+++ Exercise2_DateTime/Program.cs
@@ -10,6 +10,24 @@ namespace Exercise2_DateTime
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Write a date: (dd/MM/yyyy)");
+            var current = DateTime.Now;
+            DateTime datetime = DateTime.Parse(Console.ReadLine());
+            var diff = current - datetime;
+            Console.WriteLine("Han pasado " + diff.Days + " dias desde " + datetime.ToShortDateString());
+
+            Console.WriteLine("Write a time: (H:mm)");
+            DateTime timetime = DateTime.Parse(Console.ReadLine());
+            var difftime = current - timetime;
+            if (current.Ticks < timetime.Ticks)
+            {
+                difftime = TimeSpan.FromDays(1) + (current - timetime);
+            }
+
+            Console.WriteLine("Han pasado " + difftime.Hours + " horas y " + difftime.Minutes + " minutos desde las " + timetime.ToShortTimeString());
+
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
         }
     }
 }
